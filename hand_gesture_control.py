2c16fbd4bcab6013bb0e8437e18367475ae8c670
Mehul Sehgal <112260603+MehulSehgal@users.noreply.github.com>
2026-08-11 17:29:22 +0530

    Update hand_gesture_control.py

--- hand_gesture_control.py
+++ hand_gesture_control.py
@@ -6,12 +6,10 @@ import numpy as np
 mp_hands = mp.solutions.hands
 hands = mp_hands.Hands(max_num_hands=1, min_detection_confidence=0.7, min_tracking_confidence=0.7)
 mp_draw = mp.solutions.drawing_utils
-
 # Initialize Webcam
 cap = cv2.VideoCapture(0)
 cap.set(3, 1280) # Camera Width
 cap.set(4, 720)  # Camera Height
-
 # Variables for drawing and swiping logic
 annotations = []
 prev_x = 0
